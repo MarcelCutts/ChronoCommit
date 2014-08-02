660668ffc17fd5cae5e4212d5cb263aadf9ff47f
Marcel Cutts <me@marcelcutts.com>
2014-08-02 14:55:33 +0100

    Framework ready - bower installs all datamaps and d3/topojson requirements. Based on an angular seed system for data binding and interaction.

---
+++ app/js/controllers.js
@@ -0,0 +1,16 @@
+(function() {
+	'use strict';
+	/* Controllers */
+
+	angular.module('chronoCommit.controllers', [])
+		.controller('MyCtrl1', ['$scope',
+			function($scope) {
+
+			}
+		])
+		.controller('MyCtrl2', ['$scope',
+			function($scope) {
+
+			}
+		]);
+})();
---
+++ app/js/directives.js
@@ -0,0 +1,15 @@
+(function() {
+
+	'use strict';
+
+	/* Directives */
+
+	angular.module('chronoCommit.directives', []).
+	directive('appVersion', ['version',
+		function(version) {
+			return function(scope, elm, attrs) {
+				elm.text(version);
+			};
+		}
+	]);
+})();
