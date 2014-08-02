a198b5abf6bd9e44f5a34de452ae2e6012443a0f
Jonny Arnold <jonny.arnold89@gmail.com>
2014-08-02 22:17:02 +0100

    Added time data service

--- app/js/services.js
+++ app/js/services.js
@@ -34,5 +34,12 @@
 					fillKey: "estonia"
 				};
 			};
-		});
+		})
+	.service('timeDataService', function($http) {
+		this.commitData = $http.jsonp('/assets/hourly_commits.json')
+
+		this.getDataFor = function(day, hour) {
+			return this.commitData.filter(function(datum) { datum['day'] == day && datum['hour'] == hour })
+		}
+	});
 })();
