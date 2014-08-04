4651494a291b777c8b78bbc623573965b07bd09d
Kadi Kraman <hellokadi@gmail.com>
2014-08-04 08:52:40 +0100

    Removed test-popup

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -19,6 +19,9 @@
 					}
 				}, true);
 
+				// when a country is clicked, the clickevent will update countryClick
+				// with the ID of the currently selected country
+				// use this ID to launch popup with appropriate country data
 				$scope.countryClick = '';
 				$scope.$watch('countryClick', function(newValue, oldValue) {
 					debugger;
@@ -44,15 +47,6 @@
 						$scope.sliderTimeDescription = timeDataService.getTimeDescription();
 					}
 				}, true);
-			}
-		])
-		.controller('popupCtrl', ['$scope', 'ngDialog',
-			function($scope, ngDialog) {
-				$scope.open = function() {
-					ngDialog.open({
-						template: 'js/countryPopup.html'
-					});
-				};
 			}
 		]);
 })();
